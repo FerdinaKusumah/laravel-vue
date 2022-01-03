cc4a2aa1213ad62bffd5f98429ea312cba354627
Heri Prastio <heri.prastio56@gmail.com>
2022-01-03 22:07:06 +0700

    Create folder heriprastio

--- heripratio/index.php
+++ heripratio/index.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel VueJS</title>
+    <title>Document</title>
 </head>
 
 <body>
-    <h1>Tugas git eduwork Laravel VueJS</h1>
+    <p>Ini adalah direktori folder Heri Prastio</p>
+
 </body>
 
 </html>
