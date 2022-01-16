6cdcb7189f259ebf4a1bcdcf4ed417938f192536
unknown <dinnyendiany@gmail.com>
2022-01-16 14:57:25 +0700

    route and view

--- dinny/LaravelxVueJs/library/resources/views/layouts/admin.blade.php
+++ dinny/LaravelxVueJs/library/resources/views/layouts/admin.blade.php
@@ -205,10 +205,50 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+            <a href="{{ url ('home') }}" class="nav-link {{ request()->is('home')? 'active' : ' ' }}">
+              <i class="nav-icon fas fa-home"></i>
               <p>
                 Home
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url ('catalog') }}" class="nav-link {{ request()-> is ('catalog')? 'active' : ' ' }}">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Catalog
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url ('book') }}" class="nav-link {{ request()->is('book')? 'active' : ' ' }}">
+              <i class="nav-icon fas fa-book"></i>
+              <p>
+                Book
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url ('author') }}" class="nav-link {{ request()->is('author')? 'active' : ' ' }}">
+              <i class="nav-icon fas fa-user-tie"></i>
+              <p>
+                Author
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url ('publisher') }}" class="nav-link {{ request()->is('publisher')? 'active' : ' ' }}">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Publisher
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url ('member') }}" class="nav-link {{ request()->is('member')? 'active' : ' ' }}">
+              <i class="nav-icon fas fa-users"></i>
+              <p>
+                Member
               </p>
             </a>
           </li>
@@ -226,7 +266,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
+            <h1 class="m-0">@yield('header')</h1>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
