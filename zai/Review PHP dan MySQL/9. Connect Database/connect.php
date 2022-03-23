452ca9cf979afddd27ae471c478c003322a77571
rohiminzainur <rohiminzainur@gmail.com>
2022-03-23 17:43:23 +0700

    Upload Task Connect Database dengan Nama File connect.php

---
+++ zai/Review PHP dan MySQL/9. Connect Database/connect.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "perpus";
+
+// Create Connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+
+
+// Check Connection
+if (!$conn) {
+    die("Connection Failed: ". mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM buku JOIN katalog ON buku.id_katalog = katalog.id_katalog";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output Data
+    while($row = $result->fetch_assoc()) {
+        echo "<br> Isbn : ". $row["isbn"]. " | Judul Buku : ". $row["judul"]. " | Quantity : ". $row["qty_stok"]. " | Nama Katalog : ". $row["nama"];
+    }
+} else {
+    $conn->close();
+}
